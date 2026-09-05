ddbbb84825bc1c25a6d18121e7333d57068308a4
pavansagar-Atomic <pavansagar74834@gmail.com>
2026-09-05 18:34:11 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int[][]dp=new int[n+2][2];
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit=0;
+                if(buy==1){
+                profit=Math.max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+                }else{
+                    profit=Math.max(prices[ind]+dp[ind+2][1],dp[ind+1][0]);
+                }
+                dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
+        
+    }
+}
